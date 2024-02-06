487620275dfd68e916f0e62e0163146aa769ca70
Vishal Sharma <vishal2003ggg@gmail.com>
2024-02-06 11:26:33 +0530

    new files

---
+++ writing sycronous.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+
+let student = {
+    name : 'Mike',
+    age : 23,
+    department : 'English',
+    car : 'Honda'
+};
+
+let data = JSON.stringify(student,null,2);
+
+fs.writeFile('student-3.json',data,(err) => {
+    if (err) throw err;
+    console.log('Data written to file');
+});

d84e9ed14662c010114e93d8c0e21af93181e1a8
Vishal Sharma <vishal2003ggg@gmail.com>
2024-02-08 10:57:16 +0530

    new files

--- unpipe.js
+++ unpipe.js
@@ -4,7 +4,7 @@ var reader = fs.createReadStream('test.txt');
 // create a  writeable stream
 // create a writable stream
 var writer = fs.createWriteStream('test1.txt');
-write.on('unpipe',() => {
+writer.on('unpipe',() => {
     console.log('unpiping occured');
 });
 reader.unpipe(writer);
